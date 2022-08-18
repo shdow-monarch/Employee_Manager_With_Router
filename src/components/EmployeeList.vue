95bd725141de0fc8037d503028f89f9cd541cc75
shadow-monarch <invicibleshadowmonarch@gmail.com>
2022-08-18 12:45:58 +0530

    updated table width

--- src/components/EmployeeList.vue
+++ src/components/EmployeeList.vue
@@ -10,48 +10,48 @@
     </el-row>
     <el-row class="table-container">
       <el-table :data="tableData" max-height="1000px">
-        <el-table-column prop="id" :label="$t('idLabel')" width="260" align="center" fixed></el-table-column>
+        <el-table-column prop="id" :label="$t('idLabel')" width="320" align="center" fixed></el-table-column>
         <el-table-column :label="$t('personalInformationTitle')" align="center">
-          <el-table-column prop="firstName" :label="$t('firstNameLabel')" width="150"></el-table-column>
-          <el-table-column prop="lastName" :label="$t('lastNameLabel')" width="150"></el-table-column>
-          <el-table-column prop="dob" :label="$t('dobLabel')" width="150"></el-table-column>
-          <el-table-column prop="gender" :label="$t('genderLabel')" width="150"></el-table-column>
-          <el-table-column prop="mobile" :label="$t('mobileLabel')" width="150"></el-table-column>
-          <el-table-column prop="email" :label="$t('emailLabel')" width="150"></el-table-column>
-          <el-table-column prop="address.current" :label="$t('currentAddressLabel')" width="150"></el-table-column>
-          <el-table-column prop="address.permanent" :label="$t('permanentAddressLabel')" width="150"></el-table-column>
-          <el-table-column prop="city" :label="$t('cityLabel')" width="150"></el-table-column>
-          <el-table-column prop="state" :label="$t('stateLabel')" width="150"></el-table-column>
-          <el-table-column prop="zipCode" :label="$t('zipCodeLabel')" width="150"></el-table-column>
+          <el-table-column prop="firstName" :label="$t('firstNameLabel')" width="200"></el-table-column>
+          <el-table-column prop="lastName" :label="$t('lastNameLabel')" width="200"></el-table-column>
+          <el-table-column prop="dob" :label="$t('dobLabel')" width="200"></el-table-column>
+          <el-table-column prop="gender" :label="$t('genderLabel')" width="200"></el-table-column>
+          <el-table-column prop="mobile" :label="$t('mobileLabel')" width="200"></el-table-column>
+          <el-table-column prop="email" :label="$t('emailLabel')" width="200"></el-table-column>
+          <el-table-column prop="address.current" :label="$t('currentAddressLabel')" width="200"></el-table-column>
+          <el-table-column prop="address.permanent" :label="$t('permanentAddressLabel')" width="200"></el-table-column>
+          <el-table-column prop="city" :label="$t('cityLabel')" width="200"></el-table-column>
+          <el-table-column prop="state" :label="$t('stateLabel')" width="200"></el-table-column>
+          <el-table-column prop="zipCode" :label="$t('zipCodeLabel')" width="200"></el-table-column>
         </el-table-column>
         <el-table-column :label="$t('employeeDetailsTitle')" align="center">
-          <el-table-column prop="employeeDetails.department" :label="$t('departmentLabel')" width="150">
+          <el-table-column prop="employeeDetails.department" :label="$t('departmentLabel')" width="200">
           </el-table-column>
-          <el-table-column prop="employeeDetails.position" :label="$t('positionLabel')" width="150">
+          <el-table-column prop="employeeDetails.position" :label="$t('positionLabel')" width="200">
           </el-table-column>
-          <el-table-column prop="employeeDetails.skills" :label="$t('skillsLabel')" width="150">
+          <el-table-column prop="employeeDetails.skills" :label="$t('skillsLabel')" width="200">
             <template slot-scope="scope">{{ scope.row.employeeDetails.skills.join(', ') }}</template>
           </el-table-column>
-          <el-table-column prop="employeeDetails.type" :label="$t('typeLabel')" width="150">
+          <el-table-column prop="employeeDetails.type" :label="$t('typeLabel')" width="200">
           </el-table-column>
-          <el-table-column prop="employeeDetails.salary" :label="$t('salaryLabel')" width="150" align="right">
+          <el-table-column prop="employeeDetails.salary" :label="$t('salaryLabel')" width="200" align="right">
             <template slot-scope="scope">{{ scope.row.employeeDetails.salary }}</template>
           </el-table-column>
-          <el-table-column prop="employeeDetails.isContract" :label="$t('contractLabel')" width="150">
+          <el-table-column prop="employeeDetails.isContract" :label="$t('contractLabel')" width="200">
             <template slot-scope="scope">{{ scope.row.employeeDetails.isContract }}</template>
           </el-table-column>
-          <el-table-column prop="employeeDetails.contract.startDate" :label="$t('contractStartLabel')" width="150">
+          <el-table-column prop="employeeDetails.contract.startDate" :label="$t('contractStartLabel')" width="200">
           </el-table-column>
-          <el-table-column prop="employeeDetails.contract.endDate" :label="$t('contractEndLabel')" width="150">
+          <el-table-column prop="employeeDetails.contract.endDate" :label="$t('contractEndLabel')" width="200">
           </el-table-column>
         </el-table-column>
         <el-table-column :label="$t('bankDetailsTitle')" align="center">
-          <el-table-column prop="bankDetails.bank" :label="$t('bankNameLabel')" width="150"></el-table-column>
-          <el-table-column prop="bankDetails.branch" :label="$t('branchNameLabel')" width="150">
+          <el-table-column prop="bankDetails.bank" :label="$t('bankNameLabel')" width="200"></el-table-column>
+          <el-table-column prop="bankDetails.branch" :label="$t('branchNameLabel')" width="200">
           </el-table-column>
-          <el-table-column prop="bankDetails.accountNumber" :label="$t('accountNumberLabel')" width="150">
+          <el-table-column prop="bankDetails.accountNumber" :label="$t('accountNumberLabel')" width="200">
           </el-table-column>
-          <el-table-column prop="bankDetails.ifsc" :label="$t('ifscLabel')" width="150"></el-table-column>
+          <el-table-column prop="bankDetails.ifsc" :label="$t('ifscLabel')" width="200"></el-table-column>
         </el-table-column>
         <el-table-column :label="$t('tableActionsLabel')" fixed="right" width="180" align="center">
           <template slot-scope="scope">
